9918bf8732f54fbe28ff7e6bfcec74c3f984ad82
Maxime Princelle <maxime@princelle.org>
2022-03-12 17:01:56 +0100

    Initial Commit

---
+++ Shared/ParkStrasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkStrasApp.swift
+//  Shared
+//
+//  Created by Maxime Princelle on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ParkStrasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
